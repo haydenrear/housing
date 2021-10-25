c3847a302f0e32535c9885d4f5b8e4b68601b0db
Hayden Rear <hayden.rear@gmail.com>
2021-10-25 09:33:35 -0400

    externalize connection string

--- src/main/java/com/freddiemac/housing/config/MongoConfig.java
+++ src/main/java/com/freddiemac/housing/config/MongoConfig.java
@@ -2,14 +2,25 @@ package com.freddiemac.housing.config;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.connection.SslSettings;
+import com.mongodb.client.model.Indexes;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import org.bson.Document;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration{
+
+    @Value("${mongo.connection.string}")
+    private String connectionString;
 
     @Override
     protected String getDatabaseName()
@@ -21,13 +32,26 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration{
     @Override
     public MongoClient reactiveMongoClient(){
         ConnectionString connString = new ConnectionString(
-                "mongodb+srv://username:password@alpaca.olrez.mongodb.net/alpaca?retryWrites=true&w=majority"
+               connectionString
         );
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(connString)
                 .retryWrites(true)
                 .build();
-        return MongoClients.create(settings);
+
+        MongoClient client = MongoClients.create(settings);
+        Flux.from(addGeoIndex(client, "CovariateSuggestionData"))
+                .concatWith(addGeoIndex(client, "TargetSuggestionData"))
+                .subscribe();
+        return client;
+    }
+
+    private Publisher<String> addGeoIndex(MongoClient client, String covariateSuggestionData)
+    {
+        MongoCollection<Document> region = client.getDatabase(getDatabaseName())
+                .getCollection(covariateSuggestionData);
+        return Mono.from(region.createIndex(Indexes.geo2dsphere("zipPoly")))
+                .concatWith(region.createIndex(Indexes.geo2dsphere("location")));
     }
 
 
