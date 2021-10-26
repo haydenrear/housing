a9b9a0e1a42dc6cc956b2aaafd587eb0bdf0860c
Hayden Rear <hayden.rear@gmail.com>
2021-10-25 22:00:13 -0400

    factory

---
+++ src/main/java/com/freddiemac/housing/service/LocationService.java
@@ -0,0 +1,144 @@
+package com.freddiemac.housing.service;
+
+import com.freddiemac.housing.model.SuggestionData;
+import com.freddiemac.housing.repo.SuggestionRepo;
+import lombok.NoArgsConstructor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.geo.GeoJsonPolygon;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+@Service
+@Scope("prototype")
+@NoArgsConstructor
+public class LocationService<Z extends SuggestionData> {
+
+
+    @Value("${google.maps.geocode.api.key}")
+    String GEOCODEAPIKEY;
+
+    @Value("${google.maps.geocode.api.url}")
+    String GEOCODEAPIURL;
+
+    @Value("${google.maps.geocode.api.reverse.url}")
+    String GEOCODEREVERSEURL;
+
+    private WebClient.Builder webClient;
+    private SuggestionRepo<Z> suggestionRepo;
+
+    public LocationService(WebClient.Builder client, SuggestionRepo<Z> suggestionRepo) {
+        this.webClient = client;
+        this.suggestionRepo = suggestionRepo;
+    }
+
+    public Mono<Tuple2<GeoJsonPolygon,GeoJsonPoint>> setPointZip(String zip){
+        return getDataFromGoogle(zip)
+                .map(response -> parseData(response, 0));
+    }
+
+    public Mono<String> getDataFromGoogle(String byBlank){
+        return webClient
+                .build()
+                .get()
+                .uri(GEOCODEAPIURL+byBlank+"&key="+GEOCODEAPIKEY)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> getDataByReverse(String byBlank){
+        System.out.println(GEOCODEREVERSEURL+byBlank+"&key="+GEOCODEAPIKEY);
+        return webClient
+                .build()
+                .get()
+                .uri(GEOCODEREVERSEURL+byBlank+"&key="+GEOCODEAPIKEY)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Tuple2<GeoJsonPolygon, GeoJsonPoint> parseData(String dataToParse, int latLongIsTwoAddressIsZero){
+        try {
+            JSONParser jsonParser = new JSONParser();
+            JSONObject parse = (JSONObject) jsonParser.parse(dataToParse);
+            JSONArray array = (JSONArray) parse.get("results");
+            JSONObject innerObj = (JSONObject) jsonParser.parse(array.get(latLongIsTwoAddressIsZero).toString());
+            JSONObject geometry = (JSONObject) jsonParser.parse(innerObj.get("geometry").toString());
+            JSONObject bounds = (JSONObject) jsonParser.parse(geometry.get("bounds").toString());
+            JSONObject northeast = (JSONObject) jsonParser.parse(bounds.get("northeast").toString());
+            JSONObject southwest= (JSONObject) jsonParser.parse(bounds.get("southwest").toString());
+            JSONObject location = (JSONObject) jsonParser.parse(geometry.get("location").toString());
+            GeoJsonPoint northeastPoint = new GeoJsonPoint((Double) northeast.get("lng"),(Double) northeast.get("lat"));
+            GeoJsonPoint southwestPoint = new GeoJsonPoint((Double) southwest.get("lng"), (Double) southwest.get("lat"));
+            GeoJsonPoint locationPoint = new GeoJsonPoint((Double) location.get("lng"),(Double) location.get("lat"));
+            double xNorthwest = southwestPoint.getX();
+            double yNorthwest = northeastPoint.getY();
+            GeoJsonPoint northwestPoint = new GeoJsonPoint(xNorthwest, yNorthwest);
+            double xSoutheast = northeastPoint.getX();
+            double ySoutheast = southwestPoint.getY();
+            GeoJsonPoint finishLoop = new GeoJsonPoint(southwestPoint.getX(), northeastPoint.getY());
+            GeoJsonPoint southeastPoint = new GeoJsonPoint(xSoutheast, ySoutheast);
+            return Tuples.of(new GeoJsonPolygon(northwestPoint, southwestPoint, southeastPoint, northeastPoint, finishLoop), locationPoint);
+        } catch (ParseException ie) {
+            ie.printStackTrace();
+        }
+        return null;
+    }
+
+    public Flux<Z> findRegionsNearAny(String any) {
+        return getDataFromGoogle(any)
+                .map(response -> parseData(response, 0))
+                .map(Tuple2::getT1)
+                .flatMapMany(suggestionRepo::findByLocationIsWithin);
+    }
+
+    public Flux<Z> findRegionsByLongLat(String longLatitude) {
+        return getDataByReverse(longLatitude)
+                .map(response -> parseData(response, 2))
+                .map(Tuple2::getT1)
+                .map(this::getCenter)
+                .flatMapMany(suggestionRepo::findByLocationIsNearOrderByLocationDesc);
+    }
+
+    public GeoJsonPoint getCenter(GeoJsonPolygon geoJsonPolygon){
+
+        double highestX = 10000d;
+        double lowestX = -10000d;
+        double highestY = 10000d;
+        double lowestY = -10000d;
+
+        for(Point point : geoJsonPolygon.getPoints()){
+            if(point.getX() < lowestX) {
+                lowestX = point.getX();
+            }
+            if(point.getY() > highestY){
+                highestY = point.getY();
+            }
+            if(point.getX() > highestX) {
+                highestX = point.getX();
+            }
+            if(point.getY() < lowestY){
+                lowestY = point.getY();
+            }
+        }
+
+        double centerX = lowestX + ((highestX - lowestX) / 2);
+        double centerY = lowestY + ((highestY - lowestY) / 2);
+
+        return new GeoJsonPoint(centerX, centerY);
+    }
+}
--- src/test/java/com/freddiemac/housing/HousingApplicationTests.java
+++ src/test/java/com/freddiemac/housing/HousingApplicationTests.java
@@ -1,17 +1,66 @@
 package com.freddiemac.housing;
 
+import com.freddiemac.housing.repo.CovariateSuggestionRepo;
+import com.freddiemac.housing.repo.TargetSuggestionRepo;
+import com.freddiemac.housing.service.LocationService;
+import com.github.javafaker.Address;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.event.annotation.AfterTestClass;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 class HousingApplicationTests {
 
+    @Autowired
+    CovariateSuggestionRepo covariateSuggestionRepo;
+    @Autowired
+    TargetSuggestionRepo targetSuggestionRepo;
+    @Autowired
+    WebClient.Builder builder;
+    @Autowired
+    ApplicationContext applicationContext;
 
 
     @Test
     void contextLoads()
     {
+        IntStream.range(0,150).forEach(i -> {
+//            createPopulationDensity(i)
+        });
     }
+
+    @Test
+    public void testZips()
+    {
+        System.out.println(zipcodes(10));
+        var locationService = applicationContext.getBean(LocationService.class, builder, targetSuggestionRepo);
+        locationService.getDataFromGoogle("20904")
+                .subscribe(str -> {
+                    System.out.println(str);
+                });
+    }
+
+    public List<String> zipcodes(int number)
+    {
+        var locale = new Locale("en-US");
+        Faker faker = new Faker(locale);
+        return IntStream.range(0,number).mapToObj(i -> {
+            Address address = faker.address();
+            System.out.println(address);
+            return address.zipCode();
+        }).collect(Collectors.toList());
+    }
+
+
 
 
 }
